7e2916029e91bd761a0e4c230b8ae862039e4e3f
Gabriel <ggfresard@gmail.com>
2021-04-10 12:30:30 -0400

    second commit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
+import Canvas from "./Canvas"
 
 ReactDOM.render(
     <React.StrictMode>
